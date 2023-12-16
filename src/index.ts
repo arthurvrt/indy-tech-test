26a132cf583f3d36a8e0a26b833a537893e06a0d
Arthur Vrt <46626919+ArthurVEROT@users.noreply.github.com>
2023-12-16 16:52:58 +0100

    chore: add .env and use env port on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import createApp from "./app";
 
 const app = createApp();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
